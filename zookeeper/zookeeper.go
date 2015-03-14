6bc428fade6ac044234ce51d7127fd5b5b7456c3
John Meichle <jmeichle@gmail.com>
2015-03-14 12:00:41 -0400

    Publish into multiple places of specified chroot.  /containers/$NAME/$SERVICEPORT -> JSON info  /services/$TAG_NAME/$SERVICEPORT -> JSON info

--- zookeeper/zookeeper.go
+++ zookeeper/zookeeper.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/url"
 	"strconv"
-	// "fmt"
 	"time"
 	"encoding/json"
 
@@ -40,6 +39,14 @@ func (f *Factory) New(uri *url.URL) bridge.RegistryAdapter {
 		log.Println("zookeeper: creating base type path: " + uri.Path + "/containers")
 		c.Create(uri.Path + "/containers", []byte{}, 0, zk.WorldACL(zk.PermAll))
 	}
+	exists, _, err = c.Exists(uri.Path + "/services")
+	if err != nil {
+		log.Println("zookeeper: error checking if exists:", err)
+	}
+	if (! exists) {
+		log.Println("zookeeper: creating base type path: " + uri.Path + "/services")
+		c.Create(uri.Path + "/services", []byte{}, 0, zk.WorldACL(zk.PermAll))
+	}
 	return &ZkClient{client: c, path: uri.Path}
 }
 
@@ -66,6 +73,28 @@ type ZnodeBody struct {
 	Attrs map[string]string
 }
 
+func (r *ZkClient) createNode(p string, body []byte) {
+	exists, _, _ := r.client.Exists(p)
+	if !exists {
+		acl := zk.WorldACL(zk.PermAll)
+		log.Println("zookeeper: creating path " + p)
+		_, err := r.client.Create(p, body, 1, acl)
+		if err != nil {
+			log.Println("zookeeper: failed to register service:", err)
+		}
+	}
+}
+
+func (r *ZkClient) deleteNodeIfExists(p string) { 
+	exists, _, _ := r.client.Exists(p)
+	if exists {
+		log.Println("zookeeper: deleting path " + p)
+		err := r.client.Delete(p, -1)
+		if err != nil {
+			log.Println("zookeeper: failed to delete container path: ", err)
+		}
+	}
+}
 func (r *ZkClient) Register(service *bridge.Service) error {
 	// fmt.Printf("Here is service: %+v\n", service)
 	privatePort, _ := strconv.Atoi(service.Origin.ExposedPort)
@@ -76,7 +105,6 @@ func (r *ZkClient) Register(service *bridge.Service) error {
 	}
 
 	if (! exists) {
-		log.Println("zookeeper: creating service base path: " + r.path + "/" + service.Origin.ContainerHostname)
 		r.client.Create(r.path + "/containers/" + service.Origin.ContainerHostname, []byte{}, 0, acl)
 	}
 
@@ -85,30 +113,58 @@ func (r *ZkClient) Register(service *bridge.Service) error {
 	if err != nil {
 		log.Println("zookeeper: failed to json encode znode body:", err)
 	}
+
 	path := r.path + "/containers/" + service.Origin.ContainerHostname + "/" + service.Origin.ExposedPort
-	_, err = r.client.Create(path, body, 1, acl)
-	if err != nil {
-		log.Println("zookeeper: failed to register service:", err)
+	r.client.Create(path, body, 1, acl) // 1 == ephemeral
+
+	for _,tag := range service.Tags {
+		basePath := r.path + "/services/" + tag
+		r.client.Create(basePath, []byte{}, 0, acl)
+		servicePath := basePath + "/" + service.Origin.ExposedPort
+		r.client.Create(servicePath, body, 1, acl) // 1 == ephemeral
 	}
-	return err
+	return nil
 }
 
 func (r *ZkClient) Deregister(service *bridge.Service) error {
 	basePath := r.path + "/containers/" + service.Origin.ContainerHostname
 	servicePath := basePath + "/" + service.Origin.ExposedPort
-	log.Println("deregister container service path: " + servicePath)
-	err := r.client.Delete(servicePath, -1) // -1 means latest version number
+	err := r.client.Delete(servicePath, -1)
 	if err != nil {
 		log.Println("zookeeper: failed to deregister service:", err)
+	}
+
+	err = r.client.Delete(servicePath, -1) // -1 means latest version number
+	for _,tag := range service.Tags {
+		tagPath := r.path + "/services/" + tag + "/" + service.Origin.ExposedPort
+		r.deleteNodeIfExists(tagPath)
 	}
 	children, _, err := r.client.Children(basePath)
 	if len(children) == 0 {
 		log.Println("zookeeper: deregister empty container path: " + basePath)
-		err := r.client.Delete(basePath, -1)
+		r.deleteNodeIfExists(basePath)
+	}
+	for _,tag := range service.Tags {
+		tagPath := r.path + "/services/" + tag
+		tagServicePath := tagPath + "/" + service.Origin.ExposedPort
+		children, _, err := r.client.Children(tagServicePath)
 		if err != nil {
-			log.Println("zookeeper: failed to delete container path: ", err)
+			log.Println("zookeeper: r.Client.Children failed: ", err)
+		}
+		if len(children) == 0 {
+			log.Println("zookeeper: deregister empty container path: " + tagServicePath)
+			r.deleteNodeIfExists(tagServicePath)
+		}
+		children, _, err = r.client.Children(tagPath)
+		if err != nil {
+			log.Println("zookeeper: r.Client.Children failed: ", err)
+		}
+		if len(children) == 0 {
+			log.Println("zookeeper: deregister empty container path: " + tagPath)
+			r.deleteNodeIfExists(tagPath)
 		}
 	}
+
 	return err
 }
 
